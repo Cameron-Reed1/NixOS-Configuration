3de9c48e754a6147de2b67288778b9abc5f1d2d6
Cameron Reed <cameron@cam123.dev>
2024-01-09 10:29:12 -0700

    Moved to flakes

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+
+  description = "My nixos config as a flake";
+
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11"
+  };
+
+
+  outputs = { self, nixpkgs }@inputs: 
+  with
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+
+      nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = inputs;
+        modules = [
+          ./configuration.nix
+        ];
+      };
+
+    };
+  };
+
+}
