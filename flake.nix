c1463e04b01a62f9bb9b13d0e94b595b8f2c0327
Cameron Reed <cameron@cam123.dev>
2024-01-28 22:13:57 -0700

    Actually use the stable branch of home manager

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs = {
     # Stable
     nixpkgs.url = "nixpkgs/nixos-23.11";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Unstable
